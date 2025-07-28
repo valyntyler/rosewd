f4e54369dc22c875d2c81dda4233f954efc831bd
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-07-28 23:21:07 +0300

    feat: centering fretboard

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use crossterm::event::Event;
 use crossterm::event::KeyCode;
 use ratatui::DefaultTerminal;
 use ratatui::Frame;
+use ratatui::layout::*;
 use ratatui::widgets::Paragraph;
 
 fn main() -> Result<()> {
@@ -27,11 +28,28 @@ fn run(mut terminal: DefaultTerminal) -> Result<()> {
 }
 
 fn render(frame: &mut Frame) {
+    let fret = " ---|";
+    let frets = 22;
+    let strings = 6;
     let fretboard = Paragraph::new(
-        (0..6)
-            .map(|_| (0..22).map(|_| " ---|").collect::<String>() + "\n")
+        (0..strings)
+            .map(|_| (0..frets).map(|_| fret.to_owned()).collect::<String>() + "\n")
             .collect::<String>(),
     );
 
-    frame.render_widget(fretboard, frame.area());
+    let area = center(
+        frame.area(),
+        Constraint::Length(frets * fret.len() as u16),
+        Constraint::Length(strings),
+    );
+
+    frame.render_widget(fretboard, area);
+}
+
+fn center(area: Rect, horizontal: Constraint, vertical: Constraint) -> Rect {
+    let [area] = Layout::horizontal([horizontal])
+        .flex(Flex::Center)
+        .areas(area);
+    let [area] = Layout::vertical([vertical]).flex(Flex::Center).areas(area);
+    area
 }
